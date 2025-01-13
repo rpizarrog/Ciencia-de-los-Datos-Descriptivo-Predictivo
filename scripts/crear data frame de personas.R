ce23c901c0a3b3bf733d7a532301a2a4c91837c6
Rubén Pizarro Gurrola <rpizarro@itdurango.edu.mx>
2025-01-13 14:12:40 -0600

    Add files via upload

---
+++ scripts/crear data frame de personas.R
@@ -0,0 +1,27 @@
+# Crear un data frame
+# data frame es un conjunto de datos estructurados
+# contiene columnas que son las variables 
+# contiene registros que son las filas o renglones
+# Toda la estructura es un data.frame
+
+# El data.frame se llamará datos a partir de todos los arreglos
+
+edades <- c(24, 28, 30, 32, 48, 55, 54, 76, 64, 32)
+nombres <- c("Oscar", "Paty", "Lourdes", "Olga", "Salma",
+             "Lalo", "Lucy", "Brenda", "Oswaldo", "Sergio")
+feliz <- c("SI", "NO", "SI", "NO", "SI",
+           "SI", "NO", "NO", "SI", "SI")
+economia <- c(TRUE, FALSE, TRUE, FALSE, FALSE, 
+              FALSE, TRUE, FALSE, TRUE, TRUE)
+peso <- c(60.5, 78.6, 90.4, 65,6, 70.5,
+          100.90, 65.89, 45.90, 65.87) # Peso enn kgs son valores continuos
+estatura <- c(170, 176, 180.9, 190, 167, 
+              178.5, 186.90, 161.70, 170, 175.50)
+genero <- c("M", "F", "F", "F", "F",
+            "M", "F", "F", "M", "M")
+
+datos <- data.frame(nombres, edades, feliz, economia, estatura, peso, genero)
+
+
+
+
