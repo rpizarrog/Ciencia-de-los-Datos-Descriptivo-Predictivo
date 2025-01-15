7fd218b88975efc9f8d717a28fca1b69ed619247
Rubén Pizarro Gurrola <rpizarro@itdurango.edu.mx>
2025-01-15 12:24:40 -0600

    Create probando funciones.R

---
+++ scripts/probando funciones.R
@@ -0,0 +1,12 @@
+# Probando funciones
+# Las funcionesw en programación son 
+# piezas de código reutilizables o 
+# El objetivo de las funciones es hacer tareas
+# repetitrivas
+# Las funciones pueden recibir valores y 
+# Las funciones se utilizan para devolver valores
+
+# Ejemplos
+f_saludar <- function (){
+  print ("Hola estoy en la FUNCION saludar()")
+}
